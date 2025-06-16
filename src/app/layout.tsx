c2d844ed1499364e8bd1e673d9265c189daa5f52
Toledo97 <toledojr7@hotmail.com>
2025-06-16 12:11:31 -0500

    fix eslint error 4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,11 @@
 import type { ReactNode } from 'react';
-
 import type { Metadata } from 'next';
-import localFont from 'next/font/local';
-import { Gravitas_One } from 'next/font/google'
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
 import { ThemeProvider } from 'next-themes';
 
 import '@/app/globals.css';
 
 import './globals.css';
-
-export const gravitas = Gravitas_One({
-    subsets: ['latin'],
-    weight: '400'
-})
 
 export const metadata: Metadata = {
     title: 'Movie Tracking App',
