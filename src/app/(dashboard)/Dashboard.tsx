2a016fcd73dc0ef44bb63c4637a0e9eedbc68b94
Toledo97 <toledojr7@hotmail.com>
2025-06-17 22:06:06 -0500

    navbar adjustments

--- src/app/(dashboard)/Dashboard.tsx
+++ src/app/(dashboard)/Dashboard.tsx
@@ -14,7 +14,7 @@ import { cookies } from 'next/headers'
 import { getMovieCount, getWatchedMovieCount, getMovies, getWatchedMovies } from '@/app/lib/actions'
 import { calculateDBIdx } from '@/app/utils/utils'
 
-export default async function Dashboard({  currentPage, currentPage2, table }: { currentPage: number, currentPage2: number, table: string }) {
+export default async function Dashboard({ currentPage, currentPage2, table }: { currentPage: number, currentPage2: number, table: string }) {
 
     const cookieStore = await cookies();
     const hasCookie = cookieStore.has('user');
@@ -39,16 +39,16 @@ export default async function Dashboard({  currentPage, currentPage2, table }: {
     const [allMovies, allWatchedMovies] = await Promise.all(
         [
             getMovies(startIdx, endIdx, table),
-            getWatchedMovies( startIdxWatched, endIdxWatched, totalWatchedMovies)
+            getWatchedMovies(startIdxWatched, endIdxWatched, totalWatchedMovies)
         ]
     )
 
     return (
-        <main className='flex flex-col md:gap-16 gap-32'>
-            <div>
+        <main className='flex flex-col '>
+            <div className='h-20'> 
                 <Navbar />
             </div>
-            <div className="flex flex-col gap-4 p-4">
+            <div className="row-start-1flex flex-col gap-4 p-4 my-36">
                 <div>
                     <div className='flex flex-col gap-2 text-white px-4'>
                         <Suspense fallback={<Loading />}>
@@ -61,8 +61,8 @@ export default async function Dashboard({  currentPage, currentPage2, table }: {
                             </div>
                         </Suspense>
 
-                        { hasCookie && 
-                            <FilterCheck/>
+                        {hasCookie &&
+                            <FilterCheck />
                         }
 
                         <Suspense fallback={<Loading />}>
--- src/app/(dashboard)/NavBar.tsx
+++ src/app/(dashboard)/NavBar.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import { createClient } from '@/app/utils/supabase/server'
 import LoginModal, { LogoutButton } from '@/app/ui/Login-Modal'
 import { cookies } from 'next/headers'
-
+import SearchIcon from '@mui/icons-material/Search';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Navbar = async () => {
@@ -21,23 +21,22 @@ const Navbar = async () => {
     }
 
     return (
-        <div>
-            <nav className="navbar navbar-expand-lg 
+        <nav className="navbar navbar-expand-lg 
                             navbar-light bg-dark 
                             bg-opacity-75 text-light
                             fixed-top">
-                <div className="container flex flex-row justify-evenly md:justify-center gap-2">
+            <div className="flex flex-wrap w-full justify-center gap-3 md:gap-2 p-2">
 
-
-                    <LogoComponent />
+                <LogoComponent />
+                <div className='flex flex-row gap-2'>
 
                     <SearchComponent />
 
                     {!userExists ? <LoginModal /> : <LogoutButton />}
 
                 </div>
-            </nav >
-        </div >
+            </div>
+        </nav >
     );
 };
 
@@ -45,10 +44,10 @@ const Navbar = async () => {
 
 function LogoComponent() {
     return (
-        <div className='flex gap-2 items-center'>
-            <img width={50} className='invert' src={'/images/A24_logo.png'}></img>
-            <Link className="navbar-brand text-light font-bold"
+        <div className='flex gap-2 items-center' >
+            <Link className="flex flex-row text-light font-bold gap-2" style={{ textDecoration: 'none' }}
                 href="/">
+                <img width={50} className='invert' src={'/images/A24_logo.png'}></img>
                 Tracker
             </Link>
         </div>)
@@ -57,9 +56,9 @@ function LogoComponent() {
 function SearchComponent() {
     return (
         <form className="form-inline flex gap-2">
-            <input className="form-control mr-sm-2 flex-1" type="search" placeholder="Search" aria-label="Search"></input>
+            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
             <button id="" className={`Default-button text-white font-bold py-2 px-4 border-b-4 rounded`} type={"submit"}>
-                Search
+                <SearchIcon />
             </button>
         </form>)
 }
